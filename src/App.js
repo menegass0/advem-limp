08b8551120a8640a9d251f26291453381d287b3c
jaozin_farofa <victormenegasso@unisantos.br>
2023-06-21 15:10:29 -0300

    starting to do listingPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import ListingPage from './Componets/ListingPage/ListingPage';
 import MainPage from './Componets/MainPage/MainPage';
 
 function App() {
   return (
-    <MainPage />
+    <>
+      <MainPage />
+      <ListingPage />
+    </>
   );
 }
 
---
+++ src/Componets/ListingPage/ListingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const ListingPage = () => {
+  return (
+    <Container className='cont' fluid>
+
+    </Container>
+  )
+}
+
+export default ListingPage
