4dd1ce3d33fb1c06edbad198b7bb55a48713c733
jaozin_farofa <victormenegasso@unisantos.br>
2023-06-21 11:48:42 -0300

    main page done

--- src/Componets/MainPage/MainPage.jsx
+++ src/Componets/MainPage/MainPage.jsx
@@ -9,32 +9,21 @@ import './mainPage.css'
 
 const MainPage = () => {
   return (
-    <Container className='cont ps-5' fluid>
-        <Row className='info d-flex align-items-center pt-5 '>
-            <Col md={5}  className='company-info mb-auto mt-5'>
-                <h1>ADVEM LIMP</h1>
+    <Container className='cont ps-5 position-relative' fluid>
+        <div className="circle1 position-absolute"></div>
+        <div className="circle2 position-absolute"></div>
+        <Row className='info d-flex align-items-center pt-5'>
+            <Col ls={6} md={5} className='company-info mb-auto mt-5'>
+                <h1 className='mb-0'>ADVEM LIMP</h1>
                 <h3>Soluções em limpeza</h3>
                 <p className='w-75'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam eaque minima quisquam, ullam repudiandae quos.</p>
             </Col>
-            <Col md={5} className='img-col'>
-                <img src={limpSvg} alt="" />
+            <Col lg={5} md={4} className='img-col mt-5'>
+                <img src={limpSvg} />
             </Col>
         </Row>
     </Container>
 
-    // <div className="container">
-    //     <div className="info">
-    //         <div className="title">
-
-    //         </div>
-    //         <div className="info-text">
-
-    //         </div>
-    //     </div>
-    //     <div className="img-col">
-    //         <img src={limpSvg} alt="" />
-    //     </div>
-    // </div>
   )
 }
 
